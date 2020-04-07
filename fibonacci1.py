d4d923030c59dcac0b4ed9bad39fcc703e8080df
Roshan Adhikari <roshanadhikari30@gmail.com>
2020-04-07 13:09:29 -0400

    Add files via upload
    #still wrong

---
+++ fibonacci1.py
@@ -0,0 +1,15 @@
+def fibonacci1(num):
+    F0 = 0
+    F1 = 1
+    counter = 0
+    while counter < num:
+        print(F0)
+        #with open("C:/pY/pythonCode/fibo.txt","a+") as out:
+            #out.write(str(F0)+'\n')
+        F2 = F0 + F1
+        F0 = F1
+        F1 = F2
+        counter += 1 
+fibonacci1(5)
+#with open("fibo.txt","a+") as out:
+    #out.write(str(fibonacci1(5))+'\n'
