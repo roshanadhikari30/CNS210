7fafe56f429ee3821a3fcd4975faa6e9405296f5
Roshan Adhikari <roshanadhikari30@gmail.com>
2020-05-08 14:25:22 -0400

    Delete roshan-python274.py

--- roshan-python274.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://www.python.org/ftp/python/2.7.4/Python-2.7.4.tar.xz'
-#get the file in binary format
-r = requests.get(url)
-with open("roshan-python-274.tar.xz","wb") as file:
-    file.write(r.content)
-
-
