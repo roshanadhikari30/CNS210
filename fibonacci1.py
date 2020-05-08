67f1829d6f9c163a66c0ce3d0029d580b91e9a3a
Roshan Adhikari <roshanadhikari30@gmail.com>
2020-05-08 14:22:13 -0400

    Update fibonacci1.py
    I have changed the fibonacci code so that ouput will be shown in text file. Date: 5/8/2020

--- fibonacci1.py
+++ fibonacci1.py
@@ -1,11 +1,11 @@
 def fibonacci1(num):
     F0 = 0
     F1 = 1
-    counter = 0
+    counter = -1
     while counter < num:
         print(F0)
-        #with open("C:/pY/pythonCode/fibo.txt","a+") as out:
-            #out.write(str(F0)+'\n')
+        with open("C:/pY/pythonCode/fibo.txt","a+") as out:
+            out.write(str(F0)+'\n')
         F2 = F0 + F1
         F0 = F1
         F1 = F2
